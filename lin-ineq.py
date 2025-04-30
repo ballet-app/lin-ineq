b7ee22ec58bec9f3e9b7b255c95a1d105d491a4f
ballet-app <let.blanka@gmail.com>
2025-04-30 09:02:56 +0200

    Update lin-ineq.py

--- lin-ineq.py
+++ lin-ineq.py
@@ -69,7 +69,7 @@ else:
                 ax.fill_betweenx(y, -10, c / a, color=color, alpha=0.5, label=label)
             ax.plot(X_line, y, color=color, linewidth=2)
 
-    plot_halfplane(*parsed1, color='green', label=ineq1)
+    plot_halfplane(*parsed1, color='yellow', label=ineq1)
     plot_halfplane(*parsed2, color='red', label=ineq2)
 
     # Osie
